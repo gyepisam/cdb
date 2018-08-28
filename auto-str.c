5fe29e875a28edb0d045f749f6c5ef69bb74ea36
Gyepi Sam <self@gyepi.com>
2018-08-28 13:11:17 -0400

    Rename puts to avoid conflict with c function

--- auto-str.c
+++ auto-str.c
@@ -5,13 +5,13 @@
 char buf1[256];
 buffer ss1 = BUFFER_INIT(writeop,1,buf1,sizeof(buf1));
 
-void puts(s)
+void bputs(s)
 char *s;
 {
   if (buffer_puts(&ss1,s) == -1) _exit(111);
 }
 
-main(argc,argv)
+int main(argc,argv)
 int argc;
 char **argv;
 {
@@ -25,20 +25,20 @@ char **argv;
   value = argv[2];
   if (!value) _exit(100);
 
-  puts("char ");
-  puts(name);
-  puts("[] = \"\\\n");
+  bputs("char ");
+  bputs(name);
+  bputs("[] = \"\\\n");
 
   while (ch = *value++) {
-    puts("\\");
+    bputs("\\");
     octal[3] = 0;
     octal[2] = '0' + (ch & 7); ch >>= 3;
     octal[1] = '0' + (ch & 7); ch >>= 3;
     octal[0] = '0' + (ch & 7);
-    puts(octal);
+    bputs(octal);
   }
 
-  puts("\\\n\";\n");
+  bputs("\\\n\";\n");
   if (buffer_flush(&ss1) == -1) _exit(111);
   _exit(0);
 }
